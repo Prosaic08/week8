d43b9bb2bdb79e48a93550b464b8b681843b5143
Stephen Blott <stephen.blott@dcu.ie>
2020-03-04 13:34:18 +0000

    Comment out lines in hash script.

--- bin/hash.sh
+++ bin/hash.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-echo "user is $CI_PROJECT_NAMESPACE"
-echo "hash is" $( /var/script/hash-args "$CI_PROJECT_NAMESPACE" )
+# Uncomment all of these lines (and remove this line entirely).
+#
+# echo "user is $CI_PROJECT_NAMESPACE"
+# echo "hash is" $( /var/script/hash-args "$CI_PROJECT_NAMESPACE" )
