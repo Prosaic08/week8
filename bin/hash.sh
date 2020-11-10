fdfb6987e0438a75161717ec1d95d36499ff49fb
Stephen Blott <smblott@gmail.com>
2020-11-10 12:53:21 +0000

    Use better environment variable.

--- bin/hash.sh
+++ bin/hash.sh
@@ -2,5 +2,5 @@
 
 # Uncomment all of these lines (and remove this line entirely).
 #
-# echo "user is $CI_PROJECT_NAMESPACE"
-# echo "hash is" $( /var/script/hash-args "$CI_PROJECT_NAMESPACE" )
+# echo "user is $GITLAB_USER_LOGIN"
+# echo "hash is" $( /var/script/hash-args "$GITLAB_USER_LOGIN" )
