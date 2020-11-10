874b640faf254c181353130527a4e12ed614c6c3
Stephen Blott <smblott@gmail.com>
2020-11-10 12:57:51 +0000

    Better layour of hash script.

--- bin/hash.sh
+++ bin/hash.sh
@@ -3,4 +3,6 @@
 # Uncomment all of these lines (and remove this line entirely).
 #
 # echo "user is $GITLAB_USER_LOGIN"
-# echo "hash is" $( /var/script/hash-args "$GITLAB_USER_LOGIN" )
+#
+# echo "hash is..."
+# /var/script/hash-args "$GITLAB_USER_LOGIN"
